ea12b08866149d5a1e48f82b28ca9d646059ab77
dipachavan <deepa.chavan258@gmail.com>
2024-10-15 19:19:33 +0530

    test

--- src/test/java/org/automation/com/ex_selenium/Selenium001_Test.java
+++ src/test/java/org/automation/com/ex_selenium/Selenium001_Test.java
@@ -1,2 +1,16 @@
-package org.automation.com.ex_selenium;public class Selenium001_Test {
+package org.automation.com.ex_selenium;
+
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+@Test
+public class Selenium001_Test
+{
+
+        public void firstTest()
+        {
+                EdgeDriver driver=new EdgeDriver();
+                driver.get("https://app.vwo.com");
+        }
+
 }
